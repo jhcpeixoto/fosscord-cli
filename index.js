df8d3ded1c9c746b15cb24cb32c6cffe6c0f5187
jhcpeixoto <jhcpeixoto@outlook.pt>
2021-07-06 09:49:13 +0100

    Add CDN clone

--- index.js
+++ index.js
@@ -5,72 +5,99 @@ const pm2 = require("pm2");
 
 const apiUrl = "https://github.com/fosscord/fosscord-api";
 const gatewayUrl = "https://github.com/fosscord/fosscord-gateway";
+const cdnUrl = "https://github.com/fosscord/fosscord-cdn";
 
 function init() {
-    console.log("Cloning the repos...");
-    exec(
-        "git clone " + apiUrl + " api && git clone " + gatewayUrl + " gateway"
-    );
-    console.log("Installing dependancies...");
-    exec("cd api && npm i && cd ../gateway && npm i");
-    console.log(
-        "NOTE: You should complete the installation with the adding of the database (see https://docs.fosscord.com/contributing.html#mongodb for instructions)."
-    );
+  console.log("Cloning the repos...");
+  exec(
+    "git clone " +
+      apiUrl +
+      " api && git clone " +
+      gatewayUrl +
+      " gateway && git clone " +
+      cdnUrl +
+      " cdn"
+  );
+  console.log("Installing dependancies...");
+  exec("cd api && npm i && cd ../gateway && npm i");
+  console.log(
+    "NOTE: You should complete the installation with the adding of the database (see https://docs.fosscord.com/contributing.html#mongodb for instructions)."
+  );
 }
 program.command("init").description("initialize the instance").action(init);
 
 function start() {
-    pm2.connect(function (err) {
-        if (err) {
-            console.error(err);
-            process.exit(2);
-        }
+  pm2.connect(function (err) {
+    if (err) {
+      console.error(err);
+      process.exit(2);
+    }
 
-        console.log("Starting API...");
-        pm2.start(
-            {
-                name: "fosscord-api",
-                script: "cd api && npm run start",
-                exec_mode: "cluster",
-            },
-            function (err, apps) {
-                pm2.disconnect();
-                if (err) throw err;
-            }
-        );
+    console.log("Starting API...");
+    pm2.start(
+      {
+        name: "fosscord-api",
+        script: "cd api && npm run start",
+        exec_mode: "cluster",
+      },
+      function (err, apps) {
+        pm2.disconnect();
+        if (err) throw err;
+      }
+    );
 
-        console.log("Starting gateway...");
-        pm2.start(
-            {
-                name: "fosscord-gateway",
-                script: "cd gateway && npm run start",
-                exec_mode: "cluster",
-            },
-            function (err, apps) {
-                pm2.disconnect();
-                if (err) throw err;
-            }
-        );
-    });
+    console.log("Starting gateway...");
+    pm2.start(
+      {
+        name: "fosscord-gateway",
+        script: "cd gateway && npm run start",
+        exec_mode: "cluster",
+      },
+      function (err, apps) {
+        pm2.disconnect();
+        if (err) throw err;
+      }
+    );
+
+    console.log("Starting cdn...");
+    pm2.start(
+      {
+        name: "fosscord-cdn",
+        script: "cd cdn && npm run start",
+        exec_mode: "cluster",
+      },
+      function (err, apps) {
+        pm2.disconnect();
+        if (err) throw err;
+      }
+    );
+  });
 }
 program.command("start").description("start the instance").action(start);
 
 function stop() {
-    console.log("Stopping API...");
-    pm2.stop("fosscord-api", (err, proc) => {
-        if (err) throw err;
-    });
+  console.log("Stopping API...");
+  pm2.stop("fosscord-api", (err, proc) => {
+    if (err) throw err;
+  });
 
-    console.log("Stopping gateway...");
-    pm2.stop("fosscord-gateway", (err, proc) => {
-        if (err) throw err;
-    });
+  console.log("Stopping gateway...");
+  pm2.stop("fosscord-gateway", (err, proc) => {
+    if (err) throw err;
+  });
+
+  console.log("Stopping cdn...");
+  pm2.stop("fosscord-cdn", (err, proc) => {
+    if (err) throw err;
+  });
 }
 program.command("stop").description("stop the instance").action(stop);
 
 function update() {
-    console.log("Updating the instance...");
-    exec("cd api && git pull && npm i && cd ../gateway && git pull && npm i");
+  console.log("Updating the instance...");
+  exec(
+    "cd api && git pull && npm i && cd ../gateway && git pull && npm i && cd ../cdn && git pull && npm i"
+  );
 }
 program.command("update").description("update the instance").action(update);
 
